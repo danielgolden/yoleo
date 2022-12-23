831acbda579126e87355ce69f75e498c9fe09b24
danielgolden <danielgolden90@gmail.com>
2022-12-23 11:11:53 -0500

    Fix menu default state

--- src/store.ts
+++ src/store.ts
@@ -9,7 +9,7 @@ export const store = reactive({
       mostRecentWordIndex: 0,
     },
   ],
-  mainMenuOpen: true,
+  mainMenuOpen: false,
   currentWordIndex: 0,
   currentWordListIndex: 0,
   currentWordListReviewUnit: new wordListReviewUnit(["default", "bro"]),
